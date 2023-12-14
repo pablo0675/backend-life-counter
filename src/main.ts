e89f48fc24c74a9df34e5dfddf728b390265e042
pablal levy <pablo.levy@epitech.eu>
2023-12-14 15:42:48 +0100

    fix: size limit

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {ValidationPipe} from "@nestjs/common";
-import bodyParser from "body-parser";
+
+const bodyParser = require('body-parser');
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(bodyParser.json({limit: '50mb'}));
-    app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
